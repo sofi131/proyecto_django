ba02e52f3808f7b51fdcac4c91ff208ab1390929
sofi131 <sofiasanmartin131@gmail.com>
2024-04-05 19:49:36 +0200

    cambios a la vista inicial del blog

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 def post_list(request):
-    # Lógica para obtener una lista de publicaciones y pasarla al template
-    return render(request, 'blog/post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
