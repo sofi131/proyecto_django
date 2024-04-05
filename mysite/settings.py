357f1879d3c311bdba427a70f886398045a8bf44
sofi131 <sofiasanmartin131@gmail.com>
2024-04-05 19:22:30 +0200

    súper usuario y creando posts

--- mysite/settings.py
+++ mysite/settings.py
@@ -76,10 +76,15 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'task',
+        'USER': 'root',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',   # Si MySQL se ejecuta localmente
+        'PORT': '3306',        # Puerto predeterminado de MySQL
     }
 }
+
 
 
 # Password validation
