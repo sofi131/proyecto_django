1b0ca4eb17518a66541a4b3323c0715b4fd4f4ca
sofi131 <sofiasanmartin131@gmail.com>
2024-04-05 19:40:26 +0200

    haciendo las views, vista inicial

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,5 @@
 from django.shortcuts import render
 
-# Create your views here.
+def post_list(request):
+    # Lógica para obtener una lista de publicaciones y pasarla al template
+    return render(request, 'blog/post_list.html', {})
